6a9f938decc293c11033c7e0577282ba8169c229
Fred Wang <58385135+fredjywang@users.noreply.github.com>
2021-01-16 14:42:31 -0500

    webcam-ref

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 // 1. TODO - Import required model here
-// e.g. import * as tfmodel from "@tensorflow-models/tfmodel";
+import * as tf_mobilenet from "@tensorflow-models/mobilenet";
 import Webcam from "react-webcam";
 import "./App.css";
 // 2. TODO - Import drawing utility here
@@ -12,10 +12,16 @@ function App() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
+  // Create states
+  const [videoSrc, setvideoSrc] = useState(null);
+  const [prediction, setPrediction] = useState('');
+  const [probability, setProbability] = useState('');
+
   // Main function
-  const runCoco = async () => {
+  const runMobileNet = async () => {
     // 3. TODO - Load network 
-    // e.g. const net = await cocossd.load();
+    const net = await tf_mobilenet.load();
+    console.log("Successfully loaded model.");
     
     //  Loop and detect hands
     setInterval(() => {
@@ -24,42 +30,88 @@ function App() {
   };
 
   const detect = async (net) => {
-    // Check data is available
-    if (
-      typeof webcamRef.current !== "undefined" &&
-      webcamRef.current !== null &&
-      webcamRef.current.video.readyState === 4
-    ) {
-      // Get Video Properties
-      const video = webcamRef.current.video;
-      const videoWidth = webcamRef.current.video.videoWidth;
-      const videoHeight = webcamRef.current.video.videoHeight;
+    // // Check data is available
+    // if (
+    //   typeof webcamRef.current !== "undefined" &&
+    //   webcamRef.current !== null &&
+    //   webcamRef.current.video.readyState === 4
+    // ) {
+    //   // Get Video Properties
+    //   const video = webcamRef.current.video;
+    //   const videoWidth = webcamRef.current.video.videoWidth;
+    //   const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
-      webcamRef.current.video.width = videoWidth;
-      webcamRef.current.video.height = videoHeight;
+    //   // Set video width
+    //   webcamRef.current.video.width = videoWidth;
+    //   webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
-      canvasRef.current.width = videoWidth;
-      canvasRef.current.height = videoHeight;
+    //   // Set canvas height and width
+    //   canvasRef.current.width = videoWidth;
+    //   canvasRef.current.height = videoHeight;
 
       // 4. TODO - Make Detections
       // e.g. const obj = await net.detect(video);
+      const webcam = await tf.data.webcam(webcamRef.current);
+      while(true) {
+        // Capture + classify video image
+        const img = await webcam.capture();
+        const result = await net.classify(img);
+
+        // Set states
+        setPrediction(result[0].className);
+        setProbability(result[0].probability);
+
+
+        // Dispose tensor
+        img.dispose();
+
+        // Wait for next animation frame
+        // await tf.nextFrame();
+      }
 
       // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       // 5. TODO - Update drawing utility
       // drawSomething(obj, ctx)  
-    }
+    // }
   };
 
-  useEffect(()=>{runCoco()},[]);
+  useEffect(()=>{
+    runMobileNet();
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({video: true}, handleVideo, videoError);
+    }
+  },[]);
+
+
+  // Handle video function
+  function handleVideo (stream) {
+    // Update the state, triggering the component to re-render with the correct stream
+    setvideoSrc({videoSrc: window.URL.createObjectURL(stream)});
+  }
+
+  // Video error function 
+  function videoError () {
+
+  }
 
   return (
     <div className="App">
+      <>
+        {prediction && probability && (
+          <h1>
+            prediction : {prediction}
+            probability : {probability}
+          </h1>
+        )}
+      </>
       <header className="App-header">
-        <Webcam
+        <div>
+          <video src={videoSrc} autoPlay="true" ref={webcamRef}/>
+        </div>
+        {/* <Webcam
           ref={webcamRef}
           muted={true} 
           style={{
@@ -73,7 +125,7 @@ function App() {
             width: 640,
             height: 480,
           }}
-        />
+        /> */}
 
         <canvas
           ref={canvasRef}
@@ -92,6 +144,7 @@ function App() {
       </header>
     </div>
   );
+
 }
 
 export default App;
